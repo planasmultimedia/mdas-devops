6db75dde8a0bfc421b56458b323bde0abc2f0be8
MarcMultimedia <32850308+MarcMultimedia@users.noreply.github.com>
2019-11-05 11:53:40 +0100

    sessio 4 changes

---
+++ pipeline-docker.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+dependences (){
+    go get github.com/gorilla/websocket
+    go get github.com/labstack/echo
+}
+
+
+#cleanup ,  si falla devuelve "true" ya que puede fallar
+clean(){
+    rm -rf build || true 
+    ps aux | grep votingapp | awk {'print $1'} | head -l | xargs kill -9 
+}
+
+#build  
+build(){
+    mkdir build
+    if go build -o ./build ./src/votingapp ; then
+        cp -r ./src/votingapp/ui ./build
+    else
+        exit 1
+    fi
+
+    #ejecutar programa para hacer test
+    pushd build
+    #docker run --name myvotingapp -v /$(pwd)/build:/app -w //app -p 8080:80 ubuntu ./votingapp	    
+    docker build -f votingapp_docker/Dockerfile -t marcplanas/votingapp .
+    docker run --name votingapp -p 8080:80 -d marcplanas/votingapp
+    popd
+}
+
+
+
+#test
+test (){
+    curl http://localhost/vote \
+        --request POST \
+        --data '{"topics": ["dev", "ops"]}' \
+        --header "Content-Type: application/json"
+
+
+    curl http://localhost/vote \
+        --request PUT \
+        --data '{"topic": "dev"}' \
+        --header "Content-Type: application/json"
+
+
+    winner=$(curl http://localhost/vote \
+        --request DELETE \
+        --header "Content-Type: application/json" | jq -r '.winner') 
+
+    expectedWinner="dev"
+
+    if [ "$expectedWinner" == "$winner" ]; then
+        echo 'TEST PASSED'
+        return 0
+    else
+        echo 'TEST FAILED'
+        return 1
+    fi
+
+}
+
+dependences
+clean
+#GOOS esuna variable global que dice para que distribucion compila GO
+GOOS=linux build
+#test
+
+
